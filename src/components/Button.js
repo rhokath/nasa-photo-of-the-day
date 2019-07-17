74fc87891d73221717f75c40da3d77163a82d5b3
Kathryn Rhoads <46656679+rhokath@users.noreply.github.com>
2019-07-17 18:49:20 -0400

    working on another component

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Button(){
+    return(
+        <button>
+            this is my button
+        </button>
+    )
+}
+
+export default Button;
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-function Title({title}){
+function Title({title, date}){
     return(
         <div className="title-card">
-            this is my title
-            {title}
+           <h2>Daily Dose of NASA</h2>
+            <h1>"{title}"</h1>
+            <h3>{date}</h3>
         </div>
     )
 }
