47b8001eefc14467f5117cd3a94308709ecb5bc3
Kathryn Rhoads <46656679+rhokath@users.noreply.github.com>
2019-07-18 17:09:51 -0400

    added some styling

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import { Header1 }from "./StyledComponents";
 function Title({title, date}){
     return(
         <div className="title-card">
            <h2>Daily Dose of NASA</h2>
-            <h1>"{title}"</h1>
+            <Header1>"{title}"</Header1>
             <h3>{date}</h3>
         </div>
     )
